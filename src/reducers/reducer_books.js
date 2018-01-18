9781e8fd99fee578c016c4dc59366887cfc08140
Patryk Laba <patryk@labski.it>
2018-01-18 21:07:21 +0100

    Created books reducer

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+    return [
+        { title: 'JS: The good parts'},
+        { title: 'Node.js in action'},
+        { title: 'Grama to nie Drama'},
+        { title: 'Eloquent JS'},
+    ];
+}

cef6be13bfb679286a9dfa5d15dc2341d0f5a49f
Patryk Laba <patryk@labski.it>
2018-01-22 20:33:48 +0100

    Firt container

--- src/containers/book-list.js
+++ src/containers/book-list.js
@@ -1,5 +1,7 @@
 import React, {Component} from 'react';
-
+import {connect} from 'react-redux';
+import { selectBook } from '../actions/index';
+import { bindActionCreators } from 'redux';
 
 class BookList extends Component {
     renderList() {
@@ -18,5 +20,19 @@ class BookList extends Component {
     }
 }
 
+function mapStateToProps(state) {
+    // whatever is returned will show up as props inside of BookList
+    return {
+        books: state.books
+    };
+}
 
-export default BookList;
+// anything returned from this function will end up as props on the BookList Container
+// this.props.selectBook
+function mapDispatchToProps(dispatch) {
+    // whenever the selectBook is called, the result should be passed to all our reducers 
+    // selectBook to actionCreator
+    return bindActionCreators({ selectBook: selectBook }, dispatch);
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(BookList);
